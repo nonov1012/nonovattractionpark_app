6e3edf09ea687406b07eb09d174773c6547db482
reclea <clementnoel62@gmail.com>
2025-02-26 14:44:35 +0100

    Ajout base du caroussel boutique

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -4,6 +4,7 @@ import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:nonovattractionpark_app/widgets/container_top.dart';
 
 
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -59,15 +60,40 @@ class _MyHomePageState extends State<MyHomePage> {
       // ======BOUTIQUE=========
     }else if(page == 1){
       return SafeArea(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start, // Aligne les éléments en haut
           children: [
-            ContainerTop(text: 'nuageux'),
-            ContainerTop(text: '100 000€'),
-            ContainerTop(text: '10 000'),
-          ],
-        ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ContainerTop(text: 'nuageux'),
+                ContainerTop(text: '100 000€'),
+                ContainerTop(text: '10 000'),
+              ],
+            ),
+            SizedBox(
+              height: 240,
+              child: CarouselView(
+                itemExtent: 200,
+                children: [
+                  Image.network(
+                    "https://cdn.discordapp.com/attachments/1160568640542875700/1337792265610989661/SmartSelect_20250124_165018_Gallery.jpg?ex=67bfcde3&is=67be7c63&hm=2176322662472dde12985e385dd5517174719dc8dff1199f88053dd32a3026d6&",
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    "https://www.cetaces.org/wp-content/uploads/2009/07/Dauphin-bleu-et-blanc-Sc108060.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                  Image.network(
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8HP_qwU9TSUGL07XMbYluFa27GyV2BmRjg&s",
+                    fit: BoxFit.cover,
+                  ),
+                ],
+              ),
+            )
+          ])
       );
+
       // ======EMPLOYES=========
     }else if (page == 2){
       return SafeArea(
