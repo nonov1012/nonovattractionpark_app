da32790e52585817edc7f5c21655210a584bd7c5
reclea <clementnoel62@gmail.com>
2025-02-26 17:38:11 +0100

    Bouton sur les caroussels

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -71,26 +71,70 @@ class _MyHomePageState extends State<MyHomePage> {
                 ContainerTop(text: '10 000'),
               ],
             ),
-            SizedBox(
-              height: 240,
-              child: CarouselView(
-                itemExtent: 200,
-                children: [
-                  Image.network(
-                    "https://cdn.discordapp.com/attachments/1160568640542875700/1337792265610989661/SmartSelect_20250124_165018_Gallery.jpg?ex=67bfcde3&is=67be7c63&hm=2176322662472dde12985e385dd5517174719dc8dff1199f88053dd32a3026d6&",
-                    fit: BoxFit.cover,
-                  ),
-                  Image.network(
-                    "https://www.cetaces.org/wp-content/uploads/2009/07/Dauphin-bleu-et-blanc-Sc108060.jpg",
-                    fit: BoxFit.cover,
-                  ),
-                  Image.network(
-                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8HP_qwU9TSUGL07XMbYluFa27GyV2BmRjg&s",
-                    fit: BoxFit.cover,
-                  ),
-                ],
-              ),
-            )
+          SizedBox(
+            height: 240,
+            child: CarouselView(
+              itemExtent: 200,
+              children: [
+                Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      "https://cdn.discordapp.com/attachments/1160568640542875700/1337792265610989661/SmartSelect_20250124_165018_Gallery.jpg?ex=67bfcde3&is=67be7c63&hm=2176322662472dde12985e385dd5517174719dc8dff1199f88053dd32a3026d6&",
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action pour le bouton
+                        },
+                        child: Text('50€'),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      "https://www.cetaces.org/wp-content/uploads/2009/07/Dauphin-bleu-et-blanc-Sc108060.jpg",
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Action pour le bouton
+                        },
+                        child: Text('150€'),
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk8HP_qwU9TSUGL07XMbYluFa27GyV2BmRjg&s",
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed: () {
+                        },
+                        child: Text('500€'),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
           ])
       );
 
